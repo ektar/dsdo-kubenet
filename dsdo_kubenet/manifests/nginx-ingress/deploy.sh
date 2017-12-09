ea1beb287f90dd26c18a6f5f278127826166a540
Eric Carlson <eric@ds-do.com>
2017-12-09 16:06:02 +0000

    ingress deployer working with standard resources

--- dsdo_kubenet/manifests/nginx-ingress/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for resource in namespace default-backend configmap \
-			  tcp-services-configmap udp-services-configmap \
-			  rbac with-rbac; do
-    echo $resource
-    kubectl apply -f $resource.yaml
-    done;
-
