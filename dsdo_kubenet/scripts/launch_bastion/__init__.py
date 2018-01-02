1c262f49c96fda3d26df24039ed620cdb1ebfd2d
Eric Carlson <eric@ds-do.com>
2018-01-02 04:52:39 +0000

    Fixed jinja templating for bastion service

--- dsdo_kubenet/scripts/launch_bastion/__init__.py
+++ dsdo_kubenet/scripts/launch_bastion/__init__.py
@@ -1,8 +1,10 @@
 from ...lib import log_conf
 from ...lib import kube_common
 from ...lib import arguments
+from ...lib import cluster_conf
 
 import argparse
+from jinja2 import Template
 import json
 from kubernetes import client, config
 import kubernetes.client.rest as kube_rest
@@ -17,11 +19,27 @@ from ipdb import set_trace
 log_name = "dsdo.launch_bastion"
 
 
-def launch_bastion(create_resources=True):
+def load_org_info(config):
+    log = logging.getLogger(log_name)
+
+    org_info = {}
+    
+    org_info['organization'] = config['general']['organization']
+    org_info['domain'] = config['general']['domain']
+    org_info['admin_email'] = config['general']['admin_email']
+    org_info['base_dn'] = config['ldap']['base']
+    org_info['cluster_name'] = config['kops']['cluster_name']
+
+    return org_info
+
+
+def launch_bastion(create_resources=True, config=None):
     log = logging.getLogger(log_name)
     
     manifest_dir = pl.Path(__file__).parent.parent.parent.\
       joinpath('manifests').joinpath('bastion')
+
+    org_info = load_org_info(config)
 
     resources = [
         'deployment',
@@ -33,7 +51,10 @@ def launch_bastion(create_resources=True):
     
     for resource_name in resources:
         with manifest_dir.joinpath("{}.yaml".format(resource_name)).open() as f:
-            resources = yaml.load_all(f)
+            t = Template(f.read())
+            filled_t = t.render(
+                org_info=org_info)
+            resources = yaml.load_all(filled_t)
           
             for resource in resources:
                 if create_resources:
@@ -54,10 +75,12 @@ def main():
     # parser.add_argument("-d", "--delete", help="Delete resources", action="store_true")
     args = parser.parse_args()
 
+    config_file = cluster_conf.load_config(args.config_file)
+
     config.load_kube_config()
     
     create_resources = not args.delete
-    launch_bastion(create_resources=create_resources)
+    launch_bastion(create_resources=create_resources, config=config_file)
     
     return 0
 
